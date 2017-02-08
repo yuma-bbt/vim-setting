5e9e6da6d00081f2b6cce57f2cc34c820096013b
iwamoto yuma <yuma@yumac.local>
2017-02-08 09:51:08 +0900

    fixed files

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 sudo mv vimrc ~/.vimrc
-sudo apt-get install software-properties-common
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install neovim
-sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo apt-get install software-properties-common -y 
+sudo apt-get install python-software-properties -y 
+sudo add-apt-repository ppa:neovim-ppa/unstable 
+sudo apt-get update
+sudo apt-get install neovim -y
+sudo apt-get install python-dev python-pip python3-dev python3-pip -y 
+mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --config vi
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
