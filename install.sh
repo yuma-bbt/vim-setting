e3bf544276fe8d3f9e7462185232e08dbc72e8e5
iwamoto yuma <yuma@yumac.local>
2017-02-08 10:38:51 +0900

    fixed files

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ sudo apt-get update
 sudo apt-get install neovim -y
 sudo apt-get install python-dev python-pip python3-dev python3-pip -y 
 mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim
+git clone https://github.com/Shougo/dein.vim.git   ~/.vim/dein/repos/github.com/Shougo/dein.vim
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --config vi
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --config editor
-vim -c ":call dein#install()"
 
