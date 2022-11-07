628e9797617f51df9dfac257e61b579613c61074
yuma iwamoto <yuma@bbtower.cloud>
2022-11-07 15:32:06 +0900

    [add] subcommand

--- vimconfig.go
+++ vimconfig.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"flag"
+	"os"
 )
 
+
 func main() { 
-	fmt.Println("hello world")
-	
+	SubCmd1 := flag.NewFlagSet("deploy",flag.ExitOnError)
+	testSubCmd2 := flag.NewFlagSet("pull",flag.ExitOnError)
+
+	if len(os.Args) < 2  {
+		fmt.Println("expected 'foo' or 'bar' subcommand")
+		os.Exit(1)
+	}
+	switch os.Args[1]{
+	case "deploy":
+		testSubCmd1.Parse(os.Args[2:])
+		fmt.Println("subcommand 'SubCmd1'")
+	case "pull":
+		testSubCmd2.Parse(os.Args[2:])
+		fmt.Println("subcommand 'SubCmd2'")
+	default:
+		fmt.Println("excepted ")
+	}
 }
 
 
